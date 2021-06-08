7d6123e59fe6495a7cd3103f3f46fe690b32f7ea
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-06-08 09:46:01 -0300

    feat: responsive layout and code link

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
 }
 
